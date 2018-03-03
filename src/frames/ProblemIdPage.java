602e4811f4577559bf6b19935500855be772d625
Guilherme Azevedo <gssao@iscte-iul.pt>
2018-03-03 15:27:11 +0000

    only the validName attribute was removed, it was not necessary
    the rest of the changes have to do with the formatting of the text

--- src/frames/ProblemIdPage.java
+++ src/frames/ProblemIdPage.java
@@ -24,16 +24,15 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
-public class ProblemIdPage extends SuperPage{
+public class ProblemIdPage extends SuperPage {
 
 	private static final long serialVersionUID = 1L;
 
 	private JTextField problemName;
 	private JTextArea problemDescription;
 	private JButton nextButton;
-	private boolean validName;
 
-	//Validar se começa com uma letra grande e se só tem letras e números
+	// Validar se começa com uma letra grande e se só tem letras e números
 
 	public ProblemIdPage(UserInterface userInterface) {
 		super(userInterface);
@@ -44,7 +43,6 @@ public class ProblemIdPage extends SuperPage{
 		problemName = new JTextField(30); // XXX see if 30 is a good width
 		problemDescription = new JTextArea();
 		nextButton = FrameUtils.cuteButton("Next");
-		validName = false;
 	}
 
 	@Override
@@ -58,7 +56,8 @@ public class ProblemIdPage extends SuperPage{
 		problemPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
 		problemPanel.setBackground(Color.WHITE);
 		problemPanel.add(new JLabel("Problem name:"));
-		problemName.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
+		problemName
+				.setBorder(BorderFactory.createCompoundBorder(border, BorderFactory.createEmptyBorder(0, 10, 0, 10)));
 		problemName.addKeyListener(new KeyListener() {
 
 			@Override
@@ -67,16 +66,8 @@ public class ProblemIdPage extends SuperPage{
 
 			@Override
 			public void keyReleased(KeyEvent e) {
-				try {
-					// validates the email
-					if(Pattern.matches("[a-zA-Z0-9]+", problemName.getText()) && Character.isUpperCase(problemName.getText().charAt(0))) {	
-						validName = true;
-					} else {
-						validName = false;
-					}
-				} finally {
-					nextButton.setEnabled(validName);
-				}
+				nextButton.setEnabled(Pattern.matches("[a-zA-Z0-9]+", problemName.getText())
+						&& Character.isUpperCase(problemName.getText().charAt(0)));
 			}
 
 			@Override
@@ -95,7 +86,8 @@ public class ProblemIdPage extends SuperPage{
 		JLabel infoIcon = new JLabel();
 		infoIcon.setIcon(new ImageIcon("./src/frames/images/info_icon.png"));
 		infoPanel.add(infoIcon, BorderLayout.WEST);
-		JLabel infoLabel = new JLabel("<html>The name you choose must start with a capital letter and can only <br> contain numbers and letters (from A to Z).<br></html>");
+		JLabel infoLabel = new JLabel("<html>The name you choose must start with a capital letter and"
+				+ " can only <br> contain numbers and letters (from A to Z).<br></html>");
 		infoLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
 		infoLabel.setBorder(new EmptyBorder(0, 10, 0, 0));
 		infoPanel.add(infoLabel, BorderLayout.CENTER);
@@ -106,22 +98,22 @@ public class ProblemIdPage extends SuperPage{
 		JPanel descriptionPanel = new JPanel();
 		descriptionPanel.setLayout(new BorderLayout());
 		descriptionPanel.setBackground(Color.WHITE);
-		descriptionPanel.add(new JLabel("Problem description:  "),BorderLayout.WEST);
-		
+		descriptionPanel.add(new JLabel("Problem description:  "), BorderLayout.WEST);
+
 		problemDescription.setBackground(Color.WHITE);
 		problemDescription.setLineWrap(true);
 		problemDescription.setWrapStyleWord(true);
-	    problemDescription.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
-	    
-	    JScrollPane scrollPane = new JScrollPane(problemDescription);
+		problemDescription.setBorder(BorderFactory.createCompoundBorder(border, BorderFactory.createEmptyBorder(0, 10, 0, 10)));
+
+		JScrollPane scrollPane = new JScrollPane(problemDescription);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		//A dimensão tem que ser posta no scrollPane e não no results
-		scrollPane.setPreferredSize(new Dimension(50,70));
-	    
-	    descriptionPanel.add(scrollPane,BorderLayout.CENTER);
+		// A dimensão tem que ser posta no scrollPane e não no results
+		scrollPane.setPreferredSize(new Dimension(50, 70));
+
+		descriptionPanel.add(scrollPane, BorderLayout.CENTER);
 
 		mainPanel.add(descriptionPanel);
-		
+
 		FrameUtils.addEmptyLabels(mainPanel, 1);
 	}
 
