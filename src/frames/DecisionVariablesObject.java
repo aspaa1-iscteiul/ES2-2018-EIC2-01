2ef721dcf3668210d3461737a510a8a29da84e80
rbaao1-iscteiulpt <rbaao1@iscte-iul.pt>
2018-03-06 16:40:53 +0000

    Changed decision variables page to protect bound values with the data type selected

--- src/frames/DecisionVariablesObject.java
+++ src/frames/DecisionVariablesObject.java
@@ -21,179 +21,254 @@ import javax.swing.border.Border;
 
 public class DecisionVariablesObject {
 
-	private DecisionVariablesPage page;
-	private JPanel variablesPanel;
-	private JTextField name;
-	private final static String[] dataTypes = {"Byte", "Short", "Integer", "Long", "Float", "Double", "Boolean"};
-	private JComboBox<String> dataType;
-	private JTextField lowerBound;
-	private JTextField upperBound;
-	private final static String[] numberSets = { "N (Natural)", "Z (Integers)", "Q (Rational)", "R (Real)"};
-	private JComboBox<String> domain1;
-	private final static String[] operations = {"  ", "U", "/"};
-	private JComboBox<String> domain2;
-	private JTextField domain3;
-	private JLabel deleteIcon;
+    private DecisionVariablesPage page;
+    private JPanel variablesPanel;
+    private JTextField name;
+    private final static String[] dataTypes = {"Byte", "Short", "Integer", "Long", "Float", "Double", "Boolean"};
+    private JComboBox<String> dataType;
+    private JTextField lowerBound;
+    private JTextField upperBound;
+    private final static String[] numberSets = { "N (Natural)", "Z (Integers)", "Q (Rational)", "R (Real)"};
+    private JComboBox<String> domain1;
+    private final static String[] operations = {"  ", "U", "/"};
+    private JComboBox<String> domain2;
+    private JTextField domain3;
+    private JLabel deleteIcon;
 
-	public DecisionVariablesObject(DecisionVariablesPage page) {
-		this.page = page;
-		this.variablesPanel = new JPanel();
-		this.name = new JTextField(5);
-		this.dataType = FrameUtils.cuteComboBox(dataTypes);
-		this.lowerBound = new JTextField(5);
-		this.upperBound = new JTextField(5);
-		this.domain1 = FrameUtils.cuteComboBox(numberSets);
-		this.domain2 = FrameUtils.cuteComboBox(operations);
-		this.domain3 = new JTextField(5);
 
-		this.domain3.addKeyListener(new KeyListener() {
+    public DecisionVariablesObject(DecisionVariablesPage page) {
+	this.page = page;
+	this.variablesPanel = new JPanel();
+	this.name = new JTextField(5);
+	this.dataType = FrameUtils.cuteComboBox(dataTypes);
+	this.lowerBound = new JTextField(5);
+	this.upperBound = new JTextField(5);
+	this.domain1 = FrameUtils.cuteComboBox(numberSets);
+	this.domain2 = FrameUtils.cuteComboBox(operations);
+	this.domain3 = new JTextField(5);
 
-			@Override
-			public void keyPressed(KeyEvent arg0) {
-			}
+	this.domain3.addKeyListener(new KeyListener() {
 
-			@Override
-			public void keyReleased(KeyEvent arg0) {
-				if(Pattern.matches("[,0-9]+", domain3.getText()) || domain3.getText().isEmpty()) {
-				}else {
-					domain3.setText(domain3.getText().substring(0, domain3.getText().length() - 1));
-					JOptionPane.showMessageDialog(variablesPanel, "Only numbers or ','", "InvalidInput", JOptionPane.INFORMATION_MESSAGE);
-				}
-			}
+	    @Override
+	    public void keyPressed(KeyEvent arg0) {
+	    }
 
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-			}
+	    @Override
+	    public void keyReleased(KeyEvent arg0) {
+		if(Pattern.matches("[,0-9]+", domain3.getText()) || domain3.getText().isEmpty()) {
+		}else {
+		    domain3.setText(domain3.getText().substring(0, domain3.getText().length() - 1));
+		    JOptionPane.showMessageDialog(variablesPanel, "Only numbers or ','", "InvalidInput", JOptionPane.INFORMATION_MESSAGE);
+		}
+	    }
+
+	    @Override
+	    public void keyTyped(KeyEvent arg0) {
+	    }
+	});
+
+	this.deleteIcon = new JLabel();
+	this.deleteIcon.setIcon(new ImageIcon("./src/frames/images/delete_icon2.png"));
+
+	DecisionVariablesObject tmp = this;
+
+	this.deleteIcon.addMouseListener(new MouseListener() {
+	    @Override
+	    public void mouseClicked(MouseEvent arg0) {
+		EventQueue.invokeLater(new Runnable(){
+		    public void run(){
+			page.removeDecisionVariableFromList(tmp);
+		    }
 		});
+	    }
 
-		this.deleteIcon = new JLabel();
-		this.deleteIcon.setIcon(new ImageIcon("./src/frames/images/delete_icon2.png"));
+	    @Override
+	    public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 
-		DecisionVariablesObject tmp = this;
+	    }
 
-		this.deleteIcon.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				EventQueue.invokeLater(new Runnable(){
-					public void run(){
-						page.removeDecisionVariableFromList(tmp);
-					}
-				});
-			}
+	    @Override
+	    public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
+	    }
 
-			}
+	    @Override
+	    public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
+	    }
 
-			}
+	    @Override
+	    public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
+	    }
 
-			}
+	});
+    }
 
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-				// TODO Auto-generated method stub
+    public JPanel transformIntoAPanel() {
+	variablesPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+	variablesPanel.setBackground(Color.WHITE);
+	Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
+	name.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
+	name.setPreferredSize(new Dimension(10, 22));
+	lowerBound.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
+	lowerBound.setPreferredSize(new Dimension(10, 22));
+	upperBound.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
+	upperBound.setPreferredSize(new Dimension(10, 22));
+	domain3.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
+	domain3.setPreferredSize(new Dimension(10, 22));
+	variablesPanel.add(name);
+	variablesPanel.add(dataType);
+	variablesPanel.add(lowerBound);
+	variablesPanel.add(upperBound);
+	variablesPanel.add(domain1);
+	variablesPanel.add(domain2);
+	variablesPanel.add(domain3);
+	variablesPanel.add(deleteIcon);
+	return variablesPanel;
+    }
 
-			}
+    public JTextField getName() {
+	return name;
+    }
 
-		});
+    public void setName(JTextField name) {
+	this.name = name;
+    }
+
+    public JComboBox<String> getDataType() {
+	return dataType;
+    }
+
+    public void setDataType(JComboBox<String> dataType) {
+	this.dataType = dataType;
+    }
+
+    public JTextField getLowerBound() {
+	return lowerBound;
+    }
+
+    public void setLowerBound(JTextField lowerBound) {
+	this.lowerBound = lowerBound;
+    }
+
+    public JTextField getUpperBound() {
+	return upperBound;
+    }
+
+    public void setUpperBound(JTextField upperBound) {
+	this.upperBound = upperBound;
+    }
+
+    public JComboBox<String> getDomain1() {
+	return domain1;
+    }
+
+    public void setDomain1(JComboBox<String> domain1) {
+	this.domain1 = domain1;
+    }
+
+    public JComboBox<String> getDomain2() {
+	return domain2;
+    }
+
+    public void setDomain2(JComboBox<String> domain2) {
+	this.domain2 = domain2;
+    }
+
+    public JTextField getDomain3() {
+	return domain3;
+    }
+
+    public void setDomain3(JTextField domain3) {
+	this.domain3 = domain3;
+    }
+
+    public DecisionVariablesPage getPage() {
+	return page;
+    }
+
+    public void setPage(DecisionVariablesPage page) {
+	this.page = page;
+    }	
+
+    private boolean validateLowerBound() {
+	try {
+	    if(!name.getText().trim().isEmpty()){
+		if(dataType.getSelectedItem().toString().equals("Byte")) {
+		    Byte.parseByte(lowerBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Short")) {
+		    Short.parseShort(lowerBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Integer")) {
+		    Integer.parseInt(lowerBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Long")) {
+		    Long.parseLong(lowerBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Float")) {
+		    Float.parseFloat(lowerBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Double")) {
+		    Double.parseDouble(lowerBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Boolean")) {
+		    Boolean.parseBoolean(lowerBound.getText());
+		}
+	    }
+	}catch(Exception e) {
+	    lowerBound.setBackground(new Color(219, 151, 149).brighter());
+	    page.refreshPage();
+	    return false;
 	}
+	lowerBound.setBackground(Color.white);
+	return true;
+    }
 
-	public JPanel transformIntoAPanel() {
-		variablesPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
-		variablesPanel.setBackground(Color.WHITE);
-		Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
-		name.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
-		name.setPreferredSize(new Dimension(10, 22));
-		lowerBound.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
-		lowerBound.setPreferredSize(new Dimension(10, 22));
-		upperBound.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
-		upperBound.setPreferredSize(new Dimension(10, 22));
-		domain3.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(0, 10, 0, 10)));
-		domain3.setPreferredSize(new Dimension(10, 22));
-		variablesPanel.add(name);
-		variablesPanel.add(dataType);
-		variablesPanel.add(lowerBound);
-		variablesPanel.add(upperBound);
-		variablesPanel.add(domain1);
-		variablesPanel.add(domain2);
-		variablesPanel.add(domain3);
-		variablesPanel.add(deleteIcon);
-		return variablesPanel;
+    private boolean validateUpperBound() {
+	try {
+	    if(!name.getText().trim().isEmpty()){
+		if(dataType.getSelectedItem().toString().equals("Byte")) {
+		    Byte.parseByte(upperBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Short")) {
+		    Short.parseShort(upperBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Integer")) {
+		    Integer.parseInt(upperBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Long")) {
+		    Long.parseLong(upperBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Float")) {
+		    Float.parseFloat(upperBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Double")) {
+		    Double.parseDouble(upperBound.getText());
+		}
+		if(dataType.getSelectedItem().toString().equals("Boolean")) {
+		    Boolean.parseBoolean(upperBound.getText());
+		}
+	    }
+	}catch(Exception e) {
+	    upperBound.setBackground(new Color(219, 151, 149).brighter());
+	    page.refreshPage();
+	    return false;
 	}
+	upperBound.setBackground(Color.white);
+	return true;
+    }
 
-	public JTextField getName() {
-		return name;
+    public boolean validateData() {
+	boolean tmp = true;
+	if(validateLowerBound()==false || validateUpperBound()==false) {
+	    tmp = false;
 	}
-
-	public void setName(JTextField name) {
-		this.name = name;
-	}
-
-	public JComboBox<String> getDataType() {
-		return dataType;
-	}
-
-	public void setDataType(JComboBox<String> dataType) {
-		this.dataType = dataType;
-	}
-
-	public JTextField getLowerBound() {
-		return lowerBound;
-	}
-
-	public void setLowerBound(JTextField lowerBound) {
-		this.lowerBound = lowerBound;
-	}
-
-	public JTextField getUpperBound() {
-		return upperBound;
-	}
-
-	public void setUpperBound(JTextField upperBound) {
-		this.upperBound = upperBound;
-	}
-
-	public JComboBox<String> getDomain1() {
-		return domain1;
-	}
-
-	public void setDomain1(JComboBox<String> domain1) {
-		this.domain1 = domain1;
-	}
-
-	public JComboBox<String> getDomain2() {
-		return domain2;
-	}
-
-	public void setDomain2(JComboBox<String> domain2) {
-		this.domain2 = domain2;
-	}
-
-	public JTextField getDomain3() {
-		return domain3;
-	}
-
-	public void setDomain3(JTextField domain3) {
-		this.domain3 = domain3;
-	}
-
-	public DecisionVariablesPage getPage() {
-		return page;
-	}
-
-	public void setPage(DecisionVariablesPage page) {
-		this.page = page;
-	}	
-
-
+	return tmp;
+    }
 }
--- src/frames/DecisionVariablesPage.java
+++ src/frames/DecisionVariablesPage.java
@@ -23,228 +23,258 @@ import javax.swing.border.EmptyBorder;
 
 public class DecisionVariablesPage extends SuperPage {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private static final int standardNumberOfVariables = 3;
-	private ArrayList<DecisionVariablesObject> decisionVariableList;
-	private JPanel warningPanel;
-	private JPanel subSubMainPanel;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private static final int standardNumberOfVariables = 3;
+    private ArrayList<DecisionVariablesObject> decisionVariableList;
+    private JPanel warningPanel;
+    private JPanel warningPanel2;
+    private JPanel subSubMainPanel;
 
-	public DecisionVariablesPage(UserInterface userInterface) {
-		super(userInterface);
-		// TODO Auto-generated constructor stub
+    public DecisionVariablesPage(UserInterface userInterface) {
+	super(userInterface);
+	// TODO Auto-generated constructor stub
+    }
+
+    private JButton nextButton;
+
+    @Override
+    public void initialize() {
+	nextButton = FrameUtils.cuteButton("Next");	
+	decisionVariableList = new ArrayList<DecisionVariablesObject>();
+
+	warningPanel = new JPanel();
+	warningPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+	warningPanel.setBackground(Color.WHITE);
+	JLabel warningIcon = new JLabel();
+	warningIcon.setIcon(new ImageIcon("./src/frames/images/warning_icon.png"));
+	warningPanel.add(warningIcon);
+	JLabel warning = new JLabel("Optimization criterias must have unique names");
+	warning.setForeground(Color.red);
+	warningPanel.add(warning);
+
+	warningPanel2 = new JPanel();
+	warningPanel2.setLayout(new FlowLayout(FlowLayout.LEADING));
+	warningPanel2.setBackground(Color.WHITE);
+	JLabel warningIcon2 = new JLabel();
+	warningIcon2.setIcon(new ImageIcon("./src/frames/images/warning_icon.png"));
+	warningPanel2.add(warningIcon2);
+	JLabel warning2 = new JLabel("Lower bound and upper bound must agree with the data type selected");
+	warning2.setForeground(Color.red);
+	warningPanel2.add(warning2);
+    }
+
+    @Override
+    public void createMainPanel() {
+	mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+	// XXX change when frame size is set
+	mainPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+
+	JPanel titlePanel = new JPanel();
+	titlePanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+	titlePanel.setBackground(Color.WHITE);
+	titlePanel.add(new JLabel("Decision Variables"));
+	mainPanel.add(titlePanel);
+
+	FrameUtils.addEmptyLabels(mainPanel, 1);
+
+	JPanel subMainPanel = new JPanel();
+	subMainPanel.setBackground(Color.WHITE);
+	subMainPanel.setLayout(new BoxLayout(subMainPanel, BoxLayout.Y_AXIS));
+	subMainPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLACK, 2),
+		BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+
+	JPanel infoPanel = new JPanel();
+	infoPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+	infoPanel.setBackground(Color.WHITE);
+	JLabel name = new JLabel("Name");
+	name.setBorder(new EmptyBorder(0, 0, 0, 45)); //to add space between the labels
+	infoPanel.add(name);
+	JLabel dataType = new JLabel("Data Type");
+	dataType.setBorder(new EmptyBorder(0, 0, 0, 15));
+	infoPanel.add(dataType);
+	infoPanel.add(new JLabel("Lower Bound  "));
+	infoPanel.add(new JLabel("Upper Bound  "));
+	infoPanel.add(new JLabel("Domain"));
+
+	subMainPanel.add(infoPanel);
+
+
+	subSubMainPanel = new JPanel();
+	subSubMainPanel.setBackground(Color.WHITE);
+	subSubMainPanel.setLayout(new BoxLayout(subSubMainPanel, BoxLayout.Y_AXIS));
+
+	for(int i = 0; i != standardNumberOfVariables; i++) {
+	    DecisionVariablesObject decisionVariable = new DecisionVariablesObject(this);
+	    decisionVariableList.add(decisionVariable);
+	    subSubMainPanel.add(decisionVariable.transformIntoAPanel());
 	}
 
-	private JButton nextButton;
+	subMainPanel.add(subSubMainPanel);
 
-	@Override
-	public void initialize() {
-		nextButton = FrameUtils.cuteButton("Next");	
-		decisionVariableList = new ArrayList<DecisionVariablesObject>();
+	JPanel addOptionPanel = new JPanel();
+	addOptionPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+	addOptionPanel.setBackground(Color.WHITE);
+	JLabel addIcon = new JLabel();
+	addIcon.setIcon(new ImageIcon("./src/frames/images/add_icon.png"));
 
-		warningPanel = new JPanel();
-		warningPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
-		warningPanel.setBackground(Color.WHITE);
-		JLabel warningIcon = new JLabel();
-		warningIcon.setIcon(new ImageIcon("./src/frames/images/warning_icon.png"));
-		warningPanel.add(warningIcon);
-		JLabel warning = new JLabel("Optimization criterias must have unique names");
-		warning.setForeground(Color.red);
-		warningPanel.add(warning);
-	}
+	DecisionVariablesPage tmp = this;
 
-	@Override
-	public void createMainPanel() {
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		// XXX change when frame size is set
-		mainPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+	addIcon.addMouseListener(new MouseListener() {
 
-		JPanel titlePanel = new JPanel();
-		titlePanel.setLayout(new FlowLayout(FlowLayout.LEADING));
-		titlePanel.setBackground(Color.WHITE);
-		titlePanel.add(new JLabel("Decision Variables"));
-		mainPanel.add(titlePanel);
-
-		FrameUtils.addEmptyLabels(mainPanel, 1);
-
-		JPanel subMainPanel = new JPanel();
-		subMainPanel.setBackground(Color.WHITE);
-		subMainPanel.setLayout(new BoxLayout(subMainPanel, BoxLayout.Y_AXIS));
-		subMainPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLACK, 2),
-				BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-
-		JPanel infoPanel = new JPanel();
-		infoPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
-		infoPanel.setBackground(Color.WHITE);
-		JLabel name = new JLabel("Name");
-		name.setBorder(new EmptyBorder(0, 0, 0, 45)); //to add space between the labels
-		infoPanel.add(name);
-		JLabel dataType = new JLabel("Data Type");
-		dataType.setBorder(new EmptyBorder(0, 0, 0, 15));
-		infoPanel.add(dataType);
-		infoPanel.add(new JLabel("Lower Bound  "));
-		infoPanel.add(new JLabel("Upper Bound  "));
-		infoPanel.add(new JLabel("Domain"));
-
-		subMainPanel.add(infoPanel);
-
-
-		subSubMainPanel = new JPanel();
-		subSubMainPanel.setBackground(Color.WHITE);
-		subSubMainPanel.setLayout(new BoxLayout(subSubMainPanel, BoxLayout.Y_AXIS));
-
-		for(int i = 0; i != standardNumberOfVariables; i++) {
-			DecisionVariablesObject decisionVariable = new DecisionVariablesObject(this);
+	    @Override
+	    public void mouseClicked(MouseEvent arg0) {
+		EventQueue.invokeLater(new Runnable(){
+		    public void run(){
+			DecisionVariablesObject decisionVariable = new DecisionVariablesObject(tmp);
 			decisionVariableList.add(decisionVariable);
 			subSubMainPanel.add(decisionVariable.transformIntoAPanel());
+			validate(); //to update window
+			repaint(); //to update window
+		    }
+		});
+	    }
+
+	    @Override
+	    public void mouseEntered(MouseEvent arg0) {
+	    }
+
+	    @Override
+	    public void mouseExited(MouseEvent arg0) {
+	    }
+
+	    @Override
+	    public void mousePressed(MouseEvent arg0) {
+	    }
+
+	    @Override
+	    public void mouseReleased(MouseEvent arg0) {
+	    }
+
+	});
+	addOptionPanel.add(addIcon, BorderLayout.WEST);
+	addOptionPanel.add(new JLabel("Add new variable"));
+
+	subMainPanel.add(addOptionPanel);
+
+	JScrollPane scrollPane = new JScrollPane(subMainPanel);
+	scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+	mainPanel.add(scrollPane);
+
+	FrameUtils.addEmptyLabels(mainPanel, 1);
+
+    }
+
+    @Override
+    public void createButtonsPanel() {
+	JButton backButton = FrameUtils.cuteButton("Back");
+	backButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		userInterface.goToPreviousPage();
+	    }
+	});
+	buttonsPanel.add(backButton);
+
+	buttonsPanel.add(new JLabel()); // to add space between the two buttons
+
+	JButton cancelButton = FrameUtils.cuteButton("Cancel");
+	cancelButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		System.exit(0);
+	    }
+	});
+	buttonsPanel.add(cancelButton);
+
+	buttonsPanel.add(new JLabel()); // to add space between the two buttons
+
+	nextButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		if(verifyIfNamesAreUnique()==true && validateAllData()==true) {
+		    userInterface.goToNextPage();
+		    userInterface.setKnownSolutionsList(getKnownSolutionsFromDecisionVariables());
 		}
+	    }
+	});
+	buttonsPanel.add(nextButton);
+    }
 
-		subMainPanel.add(subSubMainPanel);
+    @Override
+    public void onTop() {
+	userInterface.getFrame().setTitle("Problem Solving App");
+    }
 
-		JPanel addOptionPanel = new JPanel();
-		addOptionPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
-		addOptionPanel.setBackground(Color.WHITE);
-		JLabel addIcon = new JLabel();
-		addIcon.setIcon(new ImageIcon("./src/frames/images/add_icon.png"));
+    public void removeDecisionVariableFromList(DecisionVariablesObject dvo) {
+	decisionVariableList.remove(dvo);
+	subSubMainPanel.remove(dvo.transformIntoAPanel());
+	refreshPage();	
+    }
 
-		DecisionVariablesPage tmp = this;
-
-		addIcon.addMouseListener(new MouseListener() {
-
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				EventQueue.invokeLater(new Runnable(){
-					public void run(){
-						DecisionVariablesObject decisionVariable = new DecisionVariablesObject(tmp);
-						decisionVariableList.add(decisionVariable);
-						subSubMainPanel.add(decisionVariable.transformIntoAPanel());
-						validate(); //to update window
-						repaint(); //to update window
-					}
-				});
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-			}
-
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-			}
-
-		});
-		addOptionPanel.add(addIcon, BorderLayout.WEST);
-		addOptionPanel.add(new JLabel("Add new variable"));
-
-		subMainPanel.add(addOptionPanel);
-
-		JScrollPane scrollPane = new JScrollPane(subMainPanel);
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-		mainPanel.add(scrollPane);
-
-		FrameUtils.addEmptyLabels(mainPanel, 1);
-
+    private ArrayList<KnownSolutionsObject> getKnownSolutionsFromDecisionVariables(){
+	ArrayList<KnownSolutionsObject> knownSolutions = new ArrayList<KnownSolutionsObject>();
+	for(DecisionVariablesObject dvo : decisionVariableList) {
+	    if(!dvo.getName().getText().trim().isEmpty()) {
+		knownSolutions.add(new KnownSolutionsObject(null, dvo.getName().getText()));
+	    }
 	}
+	return knownSolutions;
+    }
 
-	@Override
-	public void createButtonsPanel() {
-		JButton backButton = FrameUtils.cuteButton("Back");
-		backButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				userInterface.goToPreviousPage();
-			}
-		});
-		buttonsPanel.add(backButton);
-
-		buttonsPanel.add(new JLabel()); // to add space between the two buttons
-
-		JButton cancelButton = FrameUtils.cuteButton("Cancel");
-		cancelButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		buttonsPanel.add(cancelButton);
-
-		buttonsPanel.add(new JLabel()); // to add space between the two buttons
-
-		nextButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(verifyIfNamesAreUnique()==true) {
-					userInterface.goToNextPage();
-					userInterface.setKnownSolutionsList(getKnownSolutionsFromDecisionVariables());
-				}
-			}
-		});
-		buttonsPanel.add(nextButton);
-	}
-
-	@Override
-	public void onTop() {
-		userInterface.getFrame().setTitle("Problem Solving App");
-	}
-
-	public void removeDecisionVariableFromList(DecisionVariablesObject dvo) {
-		decisionVariableList.remove(dvo);
-		subSubMainPanel.remove(dvo.transformIntoAPanel());
-		refreshPage();	
-	}
-
-	private ArrayList<KnownSolutionsObject> getKnownSolutionsFromDecisionVariables(){
-		ArrayList<KnownSolutionsObject> knownSolutions = new ArrayList<KnownSolutionsObject>();
-		for(DecisionVariablesObject dvo : decisionVariableList) {
-			if(!dvo.getName().getText().trim().isEmpty()) {
-				knownSolutions.add(new KnownSolutionsObject(null, dvo.getName().getText()));
-			}
+    private boolean verifyIfNamesAreUnique() {
+	boolean var = true;
+	for(DecisionVariablesObject dvo : decisionVariableList) {
+	    String tmp = dvo.getName().getText();
+	    int count = 0;
+	    for(DecisionVariablesObject dvo2 : decisionVariableList) {
+		if(tmp.equals(dvo2.getName().getText()) && !dvo2.getName().getText().trim().isEmpty()) {
+		    count++;
+		    if(count > 1) {
+			SwingUtilities.invokeLater(new Runnable() {
+			    public void run() {
+				dvo2.getName().setBackground( new Color(219,151,149).brighter());
+				mainPanel.add(warningPanel);
+				refreshPage();
+			    }
+			});
+			var = false;
+		    } else {
+			dvo2.getName().setBackground(Color.white);
+			mainPanel.remove(warningPanel);
+		    }
 		}
-		return knownSolutions;
+	    }
 	}
+	return var;
+    }
 
-	private boolean verifyIfNamesAreUnique() {
-		boolean var = true;
-		for(DecisionVariablesObject dvo : decisionVariableList) {
-			String tmp = dvo.getName().getText();
-			int count = 0;
-			for(DecisionVariablesObject dvo2 : decisionVariableList) {
-				if(tmp.equals(dvo2.getName().getText()) && !dvo2.getName().getText().trim().isEmpty()) {
-					count++;
-					if(count > 1) {
-						SwingUtilities.invokeLater(new Runnable() {
-							public void run() {
-								dvo2.getName().setBackground( new Color(219,151,149).brighter());
-								mainPanel.add(warningPanel);
-								refreshPage();
-							}
-						});
-						var = false;
-					} else {
-						dvo2.getName().setBackground(Color.white);
-						mainPanel.remove(warningPanel);
-					}
-				}
-			}
-		}
-		return var;
+    private boolean validateAllData(){
+	boolean tmp = true;
+	for(DecisionVariablesObject dvo : decisionVariableList) {
+	    if(dvo.validateData() == false){
+		tmp = false;
+		SwingUtilities.invokeLater(new Runnable() {
+		    public void run() {
+			mainPanel.add(warningPanel2);
+			refreshPage();
+		    }
+		});
+		break;
+	    }
 	}
+	mainPanel.remove(warningPanel2);
+	refreshPage();
+	return tmp;
+    }
 
-	private void refreshPage() {
-		userInterface.getFrame().validate();
-		userInterface.getFrame().repaint();
-		userInterface.getFrame().pack();
-	}
+    public void refreshPage() {
+	userInterface.getFrame().validate();
+	userInterface.getFrame().repaint();
+	userInterface.getFrame().pack();
+    }
 
 }
